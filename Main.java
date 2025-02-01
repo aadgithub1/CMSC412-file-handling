5ebe956b4a4c9d2ff09d8ae4a1c90084aad4ae47
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-02-01 15:55:19 -0500

    Validate user input.

--- Main.java
+++ Main.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        String userChoice = getUserChoice();
+        int userChoice = getUserChoice();
         System.out.println("your choice was " + userChoice);
     }
 
-    public static String getUserChoice(){
+    public static int getUserChoice(){
+        int input = -1;
         System.out.print(
-            "0\n" +
-            "1\n" +
-            "2\n" +
-            "3\n" +
-            "4\n" +
-            "5\n"
+            "0 - Exit\n" +
+            "1 - Select directory\n" +
+            "2 - List directory content\n" +
+            "3 - Display file\n" +
+            "4 - Delete file\n" +
+            "5 - Mirror reflect file\n"
         );
         System.out.print("Select option: ");
-        return scanner.nextLine();
+        try{
+            input = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException nfe){
+            System.out.println("Invalid data type, please enter only "
+            + "whole numbers");
+        }
+
+        return input;
     }
 }
